844998c0de01e5066714d81cdbf53e08a57e6b2d
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-03-22 13:21:33 +0200

    added local storage function

--- js/plan.js
+++ js/plan.js
@@ -1,12 +1,15 @@
 'use strict';
 
 //declearing global verable and get element from html//
-
 let formData=document.getElementById('form');
 let textResult=document.getElementById('textResult');
 let videoResult=document.getElementById('videoResult');
 let pdfResult=document.getElementById('pdfResult');
-document.getElementById('submit');
+document.getElementById('submit');//what is this//
+let gainWeight=0;
+let loseWeight=0;
+let bodybuilding=0;
+let goalArray=[gainWeight,loseWeight,bodybuilding];
 
 //bulindg a constructor //
 
@@ -23,113 +26,143 @@ function UserData (name,age,gender,weight,height,active,goal,equipment){
 
 //>>>>>>>>>>>>>>>>>>>>>> MBI calucoluter function <<<<<<<<<<<<<<<<<<<<<<<//
 
-UserData.prototype.generatBMI=function(){
+UserData.prototype.generateBMI=function(){
   let BMI =this.weight/(this.height*this.height);
   return BMI;
 };
 
 //for test//
-// let test=new UserData('hala',25,'female',69,1.67,'lazy','loosin weight','yes');
-// test.generatBMI();
+// let test=new UserData('hala',25,'female',40,1.67,'lazy','loosin weight','yes');
+
 
 //>>>>>>>>>>>>>>>>>>>>>>> adding first sentences <<<<<<<<<<<<<<<<<<<<<<<//
 
-formData.addEventListener('submit',submitHander);
-
-UserData.prototype.bmiCases=function (event){
-  event.preventDefault();
+UserData.prototype.bmiCases=function (){
+  // event.preventDefault();
   if(this.generateBMI()<=18.5){
     let bmiResult= document.createElement('h2');
-    bmiResult.textContent= 'Hello ' + this.userName + '! Your BMI result is: ' + this.generatBMI() + '. Which means you\'re mostly underweight. Don\'t worry tough.. We\'re here to help!' ;
+    bmiResult.textContent= 'Hello ' + this.userName + '! Your BMI result is: ' + this.generateBMI() + '. Which means you\'re mostly underweight. Don\'t worry tough.. We\'re here to help!' ;
     textResult.appendChild(bmiResult);
   }
 
   if(this.generateBMI()>18.5 && this.generateBMI()<25){
     let bmiResult= document.createElement('h2');
-    bmiResult.textContent= 'Hello ' + this.userName + '! Your BMI result is: ' + this.generatBMI() + '. Which means you\'re in the normal range, Congrats!' ;
+    bmiResult.textContent= 'Hello ' + this.userName + '! Your BMI result is: ' + this.generateBMI() + '. Which means you\'re in the normal range, Congrats!' ;
     textResult.appendChild(bmiResult);
   }
 
   if(this.generateBMI()>25 && this.generateBMI()<30){
     let bmiResult= document.createElement('h2');
-    bmiResult.textContent= 'Hello ' + this.userName + '! Your BMI result is: ' + this.generatBMI() + '. Which means you\'re a little overweight. Watch your health out!' ;
+    bmiResult.textContent= 'Hello ' + this.userName + '! Your BMI result is: ' + this.generateBMI() + '. Which means you\'re a little overweight. Watch your health out!' ;
     textResult.appendChild(bmiResult);
   }
 
   if(this.generateBMI()>=30){
     let bmiResult= document.createElement('h2');
-    bmiResult.textContent= 'Hello ' + this.userName + '! Your BMI result is: ' + this.generatBMI() + '. Which means you\'re probably suffering from obesity. But don\'t worry.. We\'re here to help!' ;
+    bmiResult.textContent= 'Hello ' + this.userName + '! Your BMI result is: ' + this.generateBMI() + '. Which means you\'re probably suffering from obesity. But don\'t worry.. We\'re here to help!' ;
     textResult.appendChild(bmiResult);
   }
 };
 
 //>>>>>>>>>>>>>>>>>>>>>>> added event listener <<<<<<<<<<<<<<<<<<<<<<<//
+// formData.addEventListener('submit',submitHander);
 
-formData.addEventListener('submit',submitHander);
-function submitHander(event){
+formData.addEventListener('submit',submitHandler);
+function submitHandler(event){
   event.preventDefault();
+  userDataParse();
+  let user_input=  new UserData(event.target.name.value,event.target.age.value,event.target.gender.value,event.target.weight.value,event.target.height.value,event.target.active.value,event.target.goal.value,event.target.equipment.value);
+  user_input.bmiCases();
   ///// CASE 1 (M, L, E) /////-------------------------------------------
   if(event.target.gender.value === 'male' && event.target.goal.value === 'loseWeight' && event.target.equipment.value === 'yes' ){
     videoResult.innerHTML= '<iframe width="420" height="315"' + 'src="https://www.youtube.com/embed/tgbNymZ7vqY">' + '</iframe>';
     pdfResult.innerHTML= '<iframe width="420" height="315"' + 'src="male-body building-workout.pdf">' + '</iframe>';
+    loseWeight++;
+    goalArray.push(loseWeight);
+    // console.log(loseWeight);
   }
 
   ///// CASE 2 (M, L, W.E) /////---------------------------------------------
-  else if(this.event.target.gender.value === 'male' && this.event.target.goal.value === 'loseWeight' && this.event.target.equipment.value === 'no' ){
+  else if(event.target.gender.value === 'male' && event.target.goal.value === 'loseWeight' && event.target.equipment.value === 'no' ){
     //PLANS
+    loseWeight++;
   }
 
   ///// CASE 3 (M, G, E) /////----------------------------------------------
-  else if(this.event.target.gender.value === 'male' && this.event.target.goal.value === 'gainWeight' && this.event.target.equipment.value === 'yes' ){
+  else if(event.target.gender.value === 'male' && event.target.goal.value === 'gainWeight' && event.target.equipment.value === 'yes' ){
     //PLANS
+    gainWeight++;
   }
 
   ///// CASE 4 (M, G, W.E) /////-----------------------------------------------
-  else if(this.event.target.gender.value === 'male' && this.event.target.goal.value === 'gainWeight' && this.event.target.equipment.value === 'no' ){
+  else if(event.target.gender.value === 'male' && event.target.goal.value === 'gainWeight' && event.target.equipment.value === 'no' ){
     //PLANS
+    gainWeight++;
   }
 
   ///// CASE 5 (M, B, E) /////------------------------------------------------
-  else if(this.event.target.gender.value === 'male' && this.event.target.goal.value === 'bodybuilding' && this.event.target.equipment.value === 'yes' ){
+  else if(event.target.gender.value === 'male' && event.target.goal.value === 'bodybuilding' && event.target.equipment.value === 'yes' ){
     //PLANS
+    bodybuilding++;
   }
 
   ///// CASE 6 (M, B, W.E) /////------------------------------------------------
-  else if(this.event.target.gender.value === 'male' && this.event.target.goal.value === 'bodybuilding' && this.event.target.equipment.value === 'no' ){
+  else if(event.target.gender.value === 'male' &&event.target.goal.value === 'bodybuilding' && event.target.equipment.value === 'no' ){
     //PLANS
+    bodybuilding++;
   }
 
   ///// CASE 7 (F, L, E) /////------------------------------------------------
-  else if(this.event.target.gender.value === 'female' && this.event.target.goal.value === 'loseWeight' && this.event.target.equipment.value === 'yes' ){
+  else if(event.target.gender.value === 'female' && event.target.goal.value === 'loseWeight' && event.target.equipment.value === 'yes' ){
     //PLANS
+    loseWeight++;
   }
 
   ///// CASE 8 (F, L, W.E) /////------------------------------------------------
-  else if(this.event.target.gender.value === 'female' && this.event.target.goal.value === 'loseWeight' && this.event.target.equipment.value === 'no' ){
+  else if(event.target.gender.value === 'female' && event.target.goal.value === 'loseWeight' && event.target.equipment.value === 'no' ){
     //PLANS
+    loseWeight++;
   }
 
   ///// CASE 9 (F, G, E) /////------------------------------------------------
-  else if(this.event.target.gender.value === 'female' && this.event.target.goal.value === 'gainWeight' && this.event.target.equipment.value === 'yes' ){
+  else if(event.target.gender.value === 'female' && event.target.goal.value === 'gainWeight' && event.target.equipment.value === 'yes' ){
     //PLANS
+    gainWeight++;
   }
 
   ///// CASE 10 (F, G, W.E) /////------------------------------------------------
-  else if(this.event.target.gender.value === 'female' && this.event.target.goal.value === 'gainWeight' && this.event.target.equipment.value === 'no' ){
+  else if(event.target.gender.value === 'female' && event.target.goal.value === 'gainWeight' && event.target.equipment.value === 'no' ){
     //PLANS
+    gainWeight++;
   }
 
   ///// CASE 11 (F, B, E) /////------------------------------------------------
-  else if(this.event.target.gender.value === 'female' && this.event.target.goal.value === 'bodybuilding' && this.event.target.equipment.value === 'yes' ){
+  else if(event.target.gender.value === 'female' && event.target.goal.value === 'bodybuilding' && event.target.equipment.value === 'yes' ){
     //PLANS
+    bodybuilding++;
   }
 
   ///// CASE 12 (F, B, W.E) /////------------------------------------------------
-  else if(this.event.target.gender.value === 'female' && this.event.target.goal.value === 'bodybuilding' && this.event.target.equipment.value === 'no' ){
+  else if(event.target.gender.value === 'female' && event.target.goal.value === 'bodybuilding' && event.target.equipment.value === 'no' ){
+    bodybuilding++;
     //PLANS
   }
+  else{console.log('final else');}
+  goalArray=[gainWeight,loseWeight,bodybuilding];
+  localST();
 
   // textResult.addEventListener('submit', bmiCases);
   // let userInput= new UserData (userName,age,gender,weight,height,active,goal,equipment);
 }
 
+function localST(){
+  let userDatast=JSON.stringify(goalArray);
+  console.log('uuu');
+  localStorage.setItem('user data',userDatast);
+}
+function userDataParse(){
+  let newUserData =localStorage.getItem('user data');
+  if(newUserData){
+    UserData.goal=JSON.parse(newUserData);
+  }
+}
