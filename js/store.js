1c4057795a6617abbd6d95fbf84eb0180f58c532
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-03-22 14:12:29 +0200

    Finish store page

--- js/store.js
+++ js/store.js
@@ -1,18 +1,15 @@
 /* eslint-disable no-empty */
-let productArray = [];
+///////////////////////////Globals/////////////////////////////////
+let productsArray = [];
 let to_cart = [];
 let middle_section = document.getElementById('middle-section');
 let right_section = document.getElementById('right-section');
 let Checkout_btn_div = document.getElementById('Checkout-btn-div');
-// let left_section = document.getElementById('left-section');
-// let Supplemnts_or_Equipments = document.getElementById('Supplemnts-or-Equipments');
-// let weight_loss_maintain_gain = document.getElementById('weight-loss-maintain-gain');
 let filter_form_reset = document.getElementById('filter-form-reset');
 Checkout_btn_div.innerHTML = '';
-
 let filter_form = document.getElementById('filter-form');
 
-
+///////////////////////////Constructer/////////////////////////////////
 function Product(title, description, price, image_path, type,goal) {
   this.title = title;
   this.description= description;
@@ -20,15 +17,14 @@ function Product(title, description, price, image_path, type,goal) {
   this.image_path = `Image/products/${image_path}`;
   this.type = type;
   this.goal = goal;
-  productArray.push(this);
+  productsArray.push(this);
 }
 
+///////////////////////////Constructer prototypes methods/////////////////////////////////
 Product.prototype.render = function () {
   this.show_product(this.type);
 
 };
-
-
 Product.prototype.show_product = function (choice) {
 
   let Supplemnts_products_container = document.getElementById(`${choice}-products-container`);
@@ -64,7 +60,6 @@ Product.prototype.show_product = function (choice) {
   let test = document.createElement('p');
   test.textContent = this.goal;
   Supplemnts_product_info.appendChild(test);
-
   Supplemnts_product_info.appendChild(Supplemnts_product_info_price);
   Supplemnts_product.appendChild(Supplemnts_product_info);
 
@@ -82,24 +77,28 @@ Product.prototype.show_product = function (choice) {
   Supplemnts_product_button_div.appendChild(Supplemnts_product_button);
   Supplemnts_product.appendChild(Supplemnts_product_button_div);
   console.log(Supplemnts_product_button.id);
+};
+Product.prototype.to_localstorage = function () {
 
 };
 
 
-// let btn = document.getElementById('Supplemnts-product-button');
+///////////////////////////Event listners/////////////////////////////////
 middle_section.addEventListener('click', btnHandler);
+filter_form.addEventListener('submit', SEoptionHandler);
+filter_form_reset.addEventListener('click', FFRoptionHandler);
 
-
+///////////////////////////Event listners functions/////////////////////////////////
 function btnHandler(event) {
-  if (event.target.className === 'Supplemnts-product-button' || event.target.id === 'Equipments-product-button') {
+  if (event.target.id === 'Supplemnts-product-button' || event.target.id === 'Equipments-product-button') {
     to_cart.push(JSON.parse(event.target.value));
+    localStorage.setItem('cart', JSON.stringify(to_cart));
     let right_section_div = document.createElement('div');
     right_section_div.className = 'right-section-div';
     right_section.appendChild(right_section_div);
     let right_section_p = document.createElement('p');
     right_section_p.innerHTML = 'You have added <span style="color:#23689b;"> ' + event.target.name + ' </span> To the Cart, Succesfully.';
     right_section_div.appendChild(right_section_p);
-    // console.log(to_cart);
     if (Checkout_btn_div.innerHTML === '') {
       console.log('test');
       let a = document.createElement('a');
@@ -111,10 +110,7 @@ function btnHandler(event) {
       Checkout_btn_div.appendChild(a);
     }
   }
-
 }
-
-filter_form.addEventListener('submit', SEoptionHandler);
 
 function SEoptionHandler(event) {
   // console.log('sds')
@@ -137,35 +133,16 @@ function SEoptionHandler(event) {
     document.getElementById('Supplemnts-products-container').style.display = 'block';
   }
 }
-filter_form_reset.addEventListener('click', FFRoptionHandler);
+
 function FFRoptionHandler(event) {
   if (event.target.id === 'filter-form-reset') {
     console.log('sdfsdfsdf');
     filter_choices('block', 'block', 'block', 'block', 'block');
-
   }
 }
-//////////////
-// weight_loss_maintain_gain.addEventListener('click', WMGoptionHandler);
-
-// function WMGoptionHandler(event) {
-//   event.preventDefault();
-//   if (event.target.value === 'Weight-loss') {
-
-//   } else if (event.target.value === 'Gain-muscle') {
-
-//   } else if (event.target.value === 'Maintain-weight') {
-
-//   } else {
 
 
-//   }
-// }
-
-
-
-
-
+///////////////////////////Helper functions/////////////////////////////////
 function weight_loss_opt_f(display) {
   let weight_loss_opt = document.getElementsByClassName('Weight-loss');
   for (let c = 0; c < weight_loss_opt.length; c++) {
@@ -190,9 +167,10 @@ function filter_choices(c1, c2, c3, c4, c5) {
   weight_loss_opt_f(c3);
   weight_gain_opt_f(c4);
   weight_maintain_opt_f(c5);
-
 }
 
+
+///////////////////////////Creating objects/////////////////////////////////
 let test1 = new Product(
   'ultimate weight-gain',
   'Our gainer boasts 35g of protein and a staggering 61g of carbs, for a massive total of 446 calories per serving, supporting your goal of packing on muscle and size.',
@@ -213,10 +191,9 @@ let test2 = new Product(
   'Equipments','Weight-loss');
 
 
-// console.log(test1);
-// console.log(test2);
+///////////////////////////Calling objects/////////////////////////////////
 test1.render();
 test2.render();
 test3.render();
-console.log(productArray);
+
 
