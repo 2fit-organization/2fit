4880b81d32ce4056cfa30614d24005fcb491b0a2
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-03-23 23:32:03 +0200

    Do some responsivity

--- js/chart.js
+++ js/chart.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 'use strict';
 let chart_data = JSON.parse(localStorage.getItem('user data'));
 var ctx = document.getElementById('myChart').getContext('2d');
